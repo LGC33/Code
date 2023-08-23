d209f7669615d5e9d765b12a193389248c2d9212
LGC33 <132958755+LGC33@users.noreply.github.com>
2023-08-23 15:32:25 -0500

    Add files via upload

---
+++ Dis4.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+ public class Dis4
+ {  
+   public static void main(String[] args)  
+   {    
+     Scanner stdIn = new Scanner(System.in);    
+     double makes;  // number shots made    
+     double goal;       // number of moeny withdrawn
+     double makes
+     System.out.print("how many shots do you have to take before you leave.: ");   
+     goals = stdIn.nextDouble();    
+     System.out.print("how many shots did you make: ");    
+     makes = stdIn.nextDouble();    
+     if goals == makes();
+     {
+      System.out.print("You can leave now:")
+     }
+     else
+     {
+    System.out.print("Just keep shooting:")
+     }
+    } // end main
+  } // end class PrintPO
