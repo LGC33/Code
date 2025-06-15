419a164f5390f7e69f7e33ca16c6f464f2535c4b
LGC33 <132958755+LGC33@users.noreply.github.com>
2025-06-15 15:28:35 -0700

    Ensure newline at file end

--- quarts-to-barrels/quarts_to_barrels.py
+++ quarts-to-barrels/quarts_to_barrels.py
@@ -16,8 +16,11 @@ def main() -> None:
     remaining_quarts = total_quarts % QUARTS_PER_BARREL
     total_ounces = total_quarts * OUNCES_PER_QUART
 
-    print(f"{total_quarts} quarts is {barrels} barrel(s) and {remaining_quarts} quart(s).")
+    print(
+        f"{total_quarts} quarts is {barrels} barrel(s) and {remaining_quarts} quart(s)."
+    )
     print(f"That's {total_ounces} ounces.")
 
 if __name__ == "__main__":
     main()
+
